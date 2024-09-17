d6b0b300e7264748a9a0e65fafab5e5583da9aed
Matthew Elwin <elwin@northwestern.edu>
2024-09-17 11:23:06 -0500

    Added Debugging message

--- me495_demo/mynode.py
+++ me495_demo/mynode.py
@@ -12,6 +12,11 @@ class MyNode(Node):
         """Create MyNode."""
         super().__init__('mynode')
         self.get_logger().info('My Node')
+        self._tmr = self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        """Increment the count at a fixed frequency."""
+        self.get_logger().debug('Timer Hit')
 
 
 def main(args=None):
