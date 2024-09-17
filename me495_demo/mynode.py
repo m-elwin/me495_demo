c327cb63e0fdd2f72644293090704d41dc507b24
Matthew Elwin <elwin@northwestern.edu>
2024-09-17 11:40:06 -0500

    Added a publisher that publishes a count incremented in a timer.

--- me495_demo/mynode.py
+++ me495_demo/mynode.py
@@ -1,22 +1,34 @@
 """A demonstration Node for ME495."""
 
 
+from example_interfaces.msg import Int64
 import rclpy
 from rclpy.node import Node
 
 
 class MyNode(Node):
-    """Node that demonstrates ROS 2."""
+    """
+    Node that demonstrates ROS 2.
+
+    Publishes
+    ---------
+    count : example_interfaces/msg/Int64 - the count that is incremented
+
+    """
 
     def __init__(self):
         """Create MyNode."""
         super().__init__('mynode')
         self.get_logger().info('My Node')
+        self._pub = self.create_publisher(Int64, 'count', 10)
         self._tmr = self.create_timer(0.5, self.timer_callback)
+        self._count = 0
 
     def timer_callback(self):
         """Increment the count at a fixed frequency."""
         self.get_logger().debug('Timer Hit')
+        self._pub.publish(Int64(data=self._count))
+        self._count += 1
 
 
 def main(args=None):
