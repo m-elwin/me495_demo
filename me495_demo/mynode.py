2e88cbcc2a709b8c420e416cf90fd3960d20ddfe
Matthew Elwin <elwin@northwestern.edu>
2024-09-26 13:53:26 -0500

    Added a demonstration of parameters.

--- me495_demo/mynode.py
+++ me495_demo/mynode.py
@@ -14,12 +14,18 @@ class MyNode(Node):
     ---------
     count : example_interfaces/msg/Int64 - the count that is incremented
 
+    Parameters
+    ----------
+    increment : Integer - the amount to increment the count by each time
+
     """
 
     def __init__(self):
         """Create MyNode."""
         super().__init__('mynode')
         self.get_logger().info('My Node')
+        self.declare_parameter('increment', 1)
+        self._inc = self.get_parameter('increment').value
         self._pub = self.create_publisher(Int64, 'count', 10)
         self._tmr = self.create_timer(0.5, self.timer_callback)
         self._count = 0
@@ -28,7 +34,7 @@ class MyNode(Node):
         """Increment the count at a fixed frequency."""
         self.get_logger().debug('Timer Hit')
         self._pub.publish(Int64(data=self._count))
-        self._count += 1
+        self._count += self._inc
 
 
 def main(args=None):
